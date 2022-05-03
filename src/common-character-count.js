731edd8d80b7694df4cd40d8156f1e57962be763
Diana Isseyeva <cherepachka.d74@gmail.com>
2022-05-03 10:23:19 +0600

    feat: fix bugs

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const words = [];
+  words.push(s1, s2);
+  let res = words[0].split("");
+  // console.log(res);
+  let temp = [];
+
+  //начиная со второго слова в массиве
+  for (let i = 1, size = words.length; i < size; i++) {
+    for (let j = 0, sizeJ = words[i].length; j < sizeJ; j++) {
+      //перебираем все буквы каждого слова
+      const letter = words[i][j];
+      //ищем индекс буквы в результате
+      const tempIndex = res.indexOf(letter);
+      // если буква найдена то пушим в temp
+      if (tempIndex > -1) {
+        temp.push(letter);
+        res.splice(tempIndex, 1);
+      }
+    }
+    res = temp;
+    // console.log(temp)
+    temp = [];
+  }
+  return res.length;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let res = "";
+  const sortedArr = members.sort();
+  for (let i = 0; i < sortedArr.length; i++) {
+    if (typeof sortedArr[i] === "string") {
+      const word = sortedArr[i][0];
+      res += word[0].toUpperCase();
+    }
+  }
+  if (res === "") return false;
+  return res;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
